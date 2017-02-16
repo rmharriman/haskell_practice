0b2ec3760ad0205a5fd04429ceb0694a102728a3
Rob <rob@uberresearch.com>
2017-02-16 15:42:36 -0500

    notes on pattern matching and evaluation

--- add.hs
+++ add.hs
@@ -7,3 +7,19 @@ add a b = a + b
 -- checks patterns for matches in the order in which we specify them in our equations
 myNot True = False
 myNot False = True
+
+-- Better example of a function with different patterns
+-- Sums a list while there are contents, but returns 0 when the list is empty
+sumList (x:xs) = x + sumList xs
+sumList []     = 0
+
+-- Evaluation of sumList [1,2]:
+-- list notation [1,2] is shorthand for the expression (1:(2:[]))
+-- Begin by trying to match with the first pattern, (x:xs) (: is the list constructor but can be used to pattern match)
+-- Value (1:(2:[])) was constructed with (:) so the value matches (succeeds)
+-- Upon success, variables x and xs are bound to the constructors arguments (x is given value 1, xs 2:[])
+-- 1 + sumList 2:[] is evaluated next. it also succeeds with (x:xs) - x bound to 2 and xs to []
+-- Finally, evaluating 1 + 2 + sumList [] fails at first pattern and falls thru to the second, which returns 0
+-- Full result: 1 + (2 + (0)) or 3
+
+
