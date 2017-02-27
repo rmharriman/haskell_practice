82214996a7fa07817bbbe8c889af9ce3d446f145
Rob <rob@uberresearch.com>
2017-02-27 13:17:14 -0500

    Added ch 4 exercise solutions and partial function notes

--- ch4_exercises.hs
+++ ch4_exercises.hs
@@ -17,3 +17,29 @@ asInt_fold_error xs       = foldl charAsInt 0 xs
   where charAsInt :: Int -> Char -> Int
         charAsInt acc x | isDigit x = acc * 10 + digitToInt x
                         | otherwise = error ("Not a digit '" ++ [x] ++ "'")
+
+
+myConcat :: [[a]] -> [a]
+myConcat [] = error "Contains no contents"
+myConcat xs = foldr myAdd [] xs
+    where  myAdd :: [a] -> [a] -> [a]
+           myAdd acc ys = foldr (:) acc ys
+
+myConcat_foldr :: [[a]] -> [a]
+myConcat_foldr xs = foldr (++) [] xs
+
+myEven :: Int -> Bool
+myEven x | x `mod` 2 == 0 = True
+         | otherwise = False
+
+
+-- Exercise was to write the builtin any function using foldr or foldl
+-- clever solution uses the or function (||) and map
+-- first, map turns the list of one type into a list of bool's using the supplied predicate function
+-- then foldr runs the or function on the new list an returns the accumulator
+-- in this case the accumulator is a Bool and if one True is encountered, it will return true
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny pred xs = foldr (||) False (map pred xs)
+
+
+
